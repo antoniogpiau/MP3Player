6b5f983fa6a84518dadbf66a42d76ccee0d4799c
Ricardo Godoy <ricardofagodoy@gmail.com>
2014-05-16 19:57:46 -0300

    Exemplo simples

--- src/com/example/mp3player/MainActivity.java
+++ src/com/example/mp3player/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.mp3player;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	EditText nome;
+	Button bntNome;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-	}
+		nome = (EditText) this.findViewById(R.id.nome);
+		bntNome = (Button) this.findViewById(R.id.botaoNome);
 
+		bntNome.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(),
+						nome.getText().toString(), Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
+	}
 
 }
