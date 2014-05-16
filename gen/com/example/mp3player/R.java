7372f55db56be62d65a0faba0870c63481993964
Ricardo Godoy <ricardofagodoy@gmail.com>
2014-05-16 20:08:23 -0300

    Alteracao de layout

--- gen/com/example/mp3player/R.java
+++ gen/com/example/mp3player/R.java
@@ -43,9 +43,20 @@ public final class R {
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+         */
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f060001;
     }
 }
