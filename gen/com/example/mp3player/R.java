b9cebf04e020d3ee3186dbecea2061cf0c3bfce9
afr-team-f <fepis_123@hotmail.com>
2014-05-22 13:13:36 -0300

    Mechi no AndroidManifest pra adicionar a nova activity

--- gen/com/example/mp3player/R.java
+++ gen/com/example/mp3player/R.java
@@ -25,7 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
         public static final int btnBuscarMusica=0x7f080005;
         public static final int btnCriarAlbum=0x7f080003;
         public static final int btnExibeListaMusica=0x7f080006;
@@ -33,6 +33,7 @@ public final class R {
         public static final int btnIncluir=0x7f080002;
         public static final int container=0x7f080000;
         public static final int nome=0x7f080001;
+        public static final int textView1=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
